a23e56ae2d367dd15bb6a1afe0cd1ad405b7031d
draxvel <ntv3331998@gmail.com>
2018-08-03 16:11:02 +0300

    Added Sign Up Logic.

--- app/src/main/java/com/tkachuk/cardholderapp/ui/auth/singUp/SignUpPresenter.kt
+++ app/src/main/java/com/tkachuk/cardholderapp/ui/auth/singUp/SignUpPresenter.kt
@@ -1,8 +1,30 @@
 package com.tkachuk.cardholderapp.ui.auth.singUp
 
-class SignUpPresenter : ISignUpContract.ISignUpPresenter {
+import com.tkachuk.cardholderapp.data.AuthFireBase
+import com.tkachuk.cardholderapp.data.IAuthFireBase
+import com.tkachuk.cardholderapp.ui.auth.AuthActivity
+
+class SignUpPresenter(activity: AuthActivity) : ISignUpContract.ISignUpPresenter {
+
+    private  var iAuth: com.tkachuk.cardholderapp.ui.auth.IAuth = activity
 
     override fun signUp(login: String, password: String) {
+        AuthFireBase.signUp(login, password, callback = object : IAuthFireBase.SignUpCallback{
+            override fun onSignUp() {
+                iAuth.showMsg("SignUp")
+            }
 
+            override fun showMsg(msg: String) {
+                iAuth.showMsg(msg)
+            }
+        })
+    }
+
+    override fun showSignIn() {
+        iAuth.showSignIn()
+    }
+
+    override fun showRecoverPassword() {
+        iAuth.showRecoverPassword()
     }
 }
