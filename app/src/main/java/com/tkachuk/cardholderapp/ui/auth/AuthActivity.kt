94e8d249a302dc14e40b31a94611b27ca882ca20
draxvel <ntv3331998@gmail.com>
2018-08-03 12:20:43 +0300

    Added Sign In UI.

--- app/src/main/java/com/tkachuk/cardholderapp/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/tkachuk/cardholderapp/ui/auth/AuthActivity.kt
@@ -14,6 +14,8 @@ class AuthActivity : AppCompatActivity(), IAuth {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        showSignIn()
     }
 
     private fun replaceFragment(fragment: Fragment){
