ea2dcf7360e5250e2d3829bda099d99f6c1eda31
draxvel <ntv3331998@gmail.com>
2018-08-09 13:41:48 +0300

    Added secondary constructor.

--- app/src/main/java/com/tkachuk/cardholderapp/data/model/BusinessCard.kt
+++ app/src/main/java/com/tkachuk/cardholderapp/data/model/BusinessCard.kt
@@ -2,11 +2,28 @@ package com.tkachuk.cardholderapp.data.model
 
 import java.io.Serializable
 
-data class BusinessCard(
-        val id: Int,
-        var name: String,
-        var description: String,
-        var site: String,
-        var email: String,
-        var phone: String,
-        var location: String) :Serializable
+class BusinessCard():Serializable {
+
+    var id: String = ""
+    var name: String = ""
+    var description: String = ""
+    var site: String = ""
+    var email: String = ""
+    var phone: String = ""
+    var location: String = ""
+
+    constructor(id: String, name: String,
+                description: String,
+                site: String,
+                email: String,
+                phone: String,
+                location: String) : this() {
+        this.id = id
+        this.name = name
+        this.description = description
+        this.site = site
+        this.email = email
+        this.phone = phone
+        this.location = location
+    }
+}
